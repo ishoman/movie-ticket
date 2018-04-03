a9cd6873c63f765d606f750c96eb8bbdb314516c
Isaac Shoman and Joy Paglinawan <isaac@>
2018-04-03 15:59:13 -0700

    Add three discount types, output lowest of all three.

--- js/scripts.js
+++ js/scripts.js
@@ -6,8 +6,19 @@ function Ticket(title, time, age) {
   this.price = 10;
 }
 
+// Discount Prototypes
+Ticket.prototype.timeDiscount = function() {
+  if (this.time === "Early Bird (before Noon)") {
+    this.price -= 3;
+  } else if (this.time === "Matinee (Noon-5pm)") {
+    this.price -= 3;
+  } else {
+    this.price = 10;
+  }
+  return + this.price;
+};
 
-Ticket.prototype.adjustedPrice = function() {
+Ticket.prototype.ageDiscount = function() {
   if (this.age <= 12) {
     this.price -= 3;
   } else if (this.age >= 65) {
@@ -18,26 +29,36 @@ Ticket.prototype.adjustedPrice = function() {
   return this.price;
 };
 
+Ticket.prototype.movieDiscount = function() {
+  if (this.title === "Die Hard") {
+    this.price -= 6;
+  } else if (this.title === "Ready Player One") {
+    this.price += 2;
+  } else {
+    this.price = 10;
+  }
+  return this.price;
+};
+
+
 
 // user interface logic
 $(document).ready(function() {
   $("#ticket-price").submit(function(event){
     event.preventDefault();
-    debugger;
     var inputtedMovieTitle = $("#movie-title").val();
     var inputtedMovieTime = $("#movie-time").val();
     var inputtedAge = parseInt($("#age").val());
-
     var ticketPrice = new Ticket(inputtedMovieTitle, inputtedMovieTime, inputtedAge);
 
-
-    var adjustedPrice = ticketPrice.adjustedPrice();
-    console.log(adjustedPrice)
-    $("#show-ticket-price").show();
-    $("ul#price").append(adjustedPrice);
+    var ageDiscount = ticketPrice.ageDiscount();
+    var timeDiscount = ticketPrice.timeDiscount();
+    var movieDiscount = ticketPrice.movieDiscount();
+    var outputPrice = Math.min(ageDiscount, timeDiscount, movieDiscount);
 
     $(".movie-title").text(inputtedMovieTitle);
     $(".movie-time").text(inputtedMovieTime);
+    $("ul#price").append("$" + outputPrice);
   });
 
 });
